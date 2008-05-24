c369ef4a7f1cb2c2906b0beb3c8cd015957bba7f
leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
2008-05-24 19:43:17 +0000

    only creates default 404 and 500 templates if not exist
    git-svn-id: https://django-dbtemplates.googlecode.com/svn/trunk@32 cfb8ba98-e953-0410-9cff-959ffddf5974
    
    committer: leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
    
    --HG--
    extra : convert_revision : cf1463a650b3b4cd4776c8d8038bed0b3458f016

--- dbtemplates/management/__init__.py
+++ dbtemplates/management/__init__.py
@@ -1,5 +1,3 @@
-""" Creates the default database template objects. """
-
 from django.dispatch import dispatcher
 from django.db.models import signals
 from django.contrib.sites.models import Site
@@ -8,6 +6,7 @@ from dbtemplates.models import Template
 from dbtemplates import models as template_app
 
 def create_default_templates(app, created_models, verbosity):
+    """Creates the default database template objects."""
     try:
         site = Site.objects.get_current()
     except Site.DoesNotExist:
@@ -16,28 +15,34 @@ def create_default_templates(app, created_models, verbosity):
     if site is not None:
         if Template in created_models:
             if verbosity >= 2:
-                print "Creating example database templates for error 404 and error 500"
+                print "Creating default database templates for error 404 and 500"
 
-            template404 = Template(name="404.html", content="""
+            template404, created404 = Template.objects.get_or_create(
+                name="404.html")
+            if created404:
+                template404.content="""
 {% extends "base.html" %}
 {% load i18n %}
 {% block content %}
     <h2>{% trans 'Page not found' %}</h2>
     <p>{% trans "We're sorry, but the requested page could not be found." %}</p>
 {% endblock %}
-""")
-            template404.save()
-            template404.sites.add(site)
+"""
+                template404.save()
+                template404.sites.add(site)
 
-            template500 = Template(name="500.html",content="""
+            template500, created500 = Template.objects.get_or_create(
+                name="500.html")
+            if created500:
+                template500.content="""
 {% extends "base.html" %}
 {% load i18n %}
 {% block content %}
     <h1>{% trans 'Server Error <em>(500)</em>' %}</h1>
     <p>{% trans "There's been an error." %}</p>
 {% endblock %}
-""")
-            template500.save()
-            template500.sites.add(site)
+"""
+                template500.save()
+                template500.sites.add(site)
 
 dispatcher.connect(create_default_templates, sender=template_app, signal=signals.post_syncdb)
