32a25d85246f4dc23d7e6550a462ecf101d6e9c7
leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
2008-08-23 22:12:17 +0000

    Added Hebrew translation, thanks mkriheli
    git-svn-id: https://django-dbtemplates.googlecode.com/svn/trunk@43 cfb8ba98-e953-0410-9cff-959ffddf5974
    
    committer: leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
    
    --HG--
    extra : convert_revision : f75b729367d58618ba8670166236938f760e2633

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='dbtemplates',
-    version='0.4.1',
+    version='0.4.2',
     description='Template loader for database stored templates',
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
