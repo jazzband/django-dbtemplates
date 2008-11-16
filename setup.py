fff8405c69de8bdeb59a0c6665cad1e51da307bc
leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
2008-11-16 02:02:48 +0000

    Updated MANIFEST and bumped minor version to 0.5.2
    git-svn-id: https://django-dbtemplates.googlecode.com/svn/trunk@77 cfb8ba98-e953-0410-9cff-959ffddf5974
    
    committer: leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
    
    --HG--
    extra : convert_revision : d6a0d6246b4187ae39b4b0504be20964d46e13b9

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='django-dbtemplates',
-    version='0.5.1',
+    version='0.5.2',
     description='Template loader for database stored templates',
     long_description=open('docs/overview.txt').read(),
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
     url='http://www.bitbucket.org/jezdez/django-dbtemplates/wiki/',
-    download_url='http://www.bitbucket.org/jezdez/django-dbtemplates/get/v0.5.1.gz',
+    download_url='http://www.bitbucket.org/jezdez/django-dbtemplates/get/v0.5.2.gz',
     packages=[
         'dbtemplates',
         'dbtemplates.management',
