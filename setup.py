7fafcbd72b99e89f4666c0b54f42417ca399187c
leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
2008-11-30 00:54:35 +0000

    Small change to fix download from Github.
    git-svn-id: https://django-dbtemplates.googlecode.com/svn/trunk@87 cfb8ba98-e953-0410-9cff-959ffddf5974
    
    committer: leidel <leidel@cfb8ba98-e953-0410-9cff-959ffddf5974>
    
    --HG--
    extra : convert_revision : b0159e9f5cb18d49f224d9d5649614492f80ac49

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
     url='http://github.com/jezdez/django-dbtemplates/wikis/',
-    download_url='http://github.com/jezdez/django-dbtemplates/tarball/0.5.3',
+    download_url='http://github.com/jezdez/django-dbtemplates/zipball/0.5.3',
     packages=[
         'dbtemplates',
         'dbtemplates.management',
