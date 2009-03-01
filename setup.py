078985d62948c1ae1a802700aedc0a41d14e63f3
Jannis Leidel <jannis@leidel.info>
2009-03-01 01:04:19 +0100

    Changed setup.py to use setuptools because it has a with zip_safe attribute which prevents creating zipped installations.
    committer: Jannis Leidel <jannis@leidel.info>
    
    --HG--
    extra : convert_revision : 5e5b1e2202d8e4b2354e52273e468a829f173508

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-dbtemplates',
@@ -8,13 +8,14 @@ setup(
     author='Jannis Leidel',
     author_email='jannis@leidel.info',
     url='http://github.com/jezdez/django-dbtemplates/wikis/',
-    download_url='http://github.com/jezdez/django-dbtemplates/zipball/0.5.4',
-    packages=[
-        'dbtemplates',
-        'dbtemplates.management',
-        'dbtemplates.management.commands'
-    ],
-    package_dir={'dbtemplates': 'dbtemplates'},
+    download_url='http://cloud.github.com/downloads/jezdez/django-dbtemplates/django-dbtemplates-0.5.5.tar.gz',
+    packages=find_packages(),
+    zip_safe=False,
+    package_data = {
+        'dbtemplates': [
+            'locale/*/LC_MESSAGES/*',
+        ],
+    },
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
