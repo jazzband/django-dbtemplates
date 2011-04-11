8c603399c2b96a66d713b77d5cd63f93b158fd3b
Jannis Leidel <jannis@leidel.info>
2011-04-11 19:21:11 +0200

    Added DATABASES setting to example project to make sure we can run on modern Django.

--- example/settings.py
+++ example/settings.py
@@ -19,6 +19,13 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": DATABASE_NAME,
+    }
+}
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
