f6ea762ed4bc1c97ea92e033821ef6f91668c4fa
Jannis Leidel <jannis@leidel.info>
2011-08-15 13:12:47 +0200

    Bumped to 1.2 and extended Trove classifiers.

--- setup.py
+++ setup.py
@@ -18,12 +18,16 @@ setup(
         ],
     },
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Framework :: Django',
     ],
 )
