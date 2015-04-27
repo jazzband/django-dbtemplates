c2d49e1d71415d238ff299ec433d843de72e20e1
Michael Kutý <6du1ro.n@gmail.com>
2015-04-27 23:40:42 +0200

    hot-fixed populating template djagno 1.8+

--- dbtemplates/utils/template.py
+++ dbtemplates/utils/template.py
@@ -21,31 +21,39 @@ def get_loaders():
 
 def get_template_source(name):
     source = None
-    for loader in get_loaders():
-        if loader.__module__.startswith('dbtemplates.'):
-            # Don't give a damn about dbtemplates' own loader.
-            continue
-        module = import_module(loader.__module__)
-        load_template_source = getattr(module, 'load_template_source', None)
-        if load_template_source is None:
-            load_template_source = loader.load_template_source
-        try:
-            source, origin = load_template_source(name)
-            if source:
-                return source
-        except NotImplementedError:
-            pass
-        except TemplateDoesNotExist:
-            pass
-    if source is None and VERSION[:2] < (1, 2):
-        # Django supported template source extraction still :/
-        try:
-            from django.template.loader import find_template_source
-            template, origin = find_template_source(name, None)
-            if not hasattr(template, 'render'):
-                return template
-        except (ImportError, TemplateDoesNotExist):
-            pass
+    if VERSION[:2] < (1, 8):
+        for loader in get_loaders():
+            if loader.__module__.startswith('dbtemplates.'):
+                # Don't give a damn about dbtemplates' own loader.
+                continue
+            module = import_module(loader.__module__)
+            load_template_source = getattr(
+                module, 'load_template_source', None)
+            if load_template_source is None:
+                load_template_source = loader.load_template_source
+            try:
+                source, origin = load_template_source(name)
+                if source:
+                    return source
+            except NotImplementedError:
+                pass
+            except TemplateDoesNotExist:
+                pass
+        if source is None and VERSION[:2] < (1, 2):
+            # Django supported template source extraction still :/
+            try:
+                from django.template.loader import find_template_source
+                template, origin = find_template_source(name, None)
+                if not hasattr(template, 'render'):
+                    return template
+            except (ImportError, TemplateDoesNotExist):
+                pass
+    else:
+        from django.template.loader import get_template
+        template = get_template('base/page/adminlte.html')
+        with open(str(template.origin)) as source_file:
+            source = source_file.read()
+        return source
     return None
 
 
