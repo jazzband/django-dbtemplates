266988dde56e72a6b81a11f19380766d993c1ff1
user.name <gmail@hdknr.com>
2021-06-03 10:30:25 +0900

    default_auto_field

--- dbtemplates/apps.py
+++ dbtemplates/apps.py
@@ -3,5 +3,6 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class DBTemplatesConfig(AppConfig):
-    name = 'dbtemplates'
-    verbose_name = _('Database templates')
+    name = "dbtemplates"
+    verbose_name = _("Database templates")
+    default_auto_field = "django.db.models.AutoField"
