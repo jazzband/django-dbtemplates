9a27f4938c3e29a47b3a6f7007c1757da285252e
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-06-15 15:41:33 +0300

    Drop support for EOL Python 3.6

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
             "static/dbtemplates/js/*.js",
         ],
     },
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
@@ -36,7 +37,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
